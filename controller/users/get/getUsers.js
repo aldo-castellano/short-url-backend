041700f20513ff1bc1765d6148c0246731270d75
aldo <aldofcastellanop@gmail.com>
2022-05-12 14:49:54 +0200

    getUsers

--- controller/users/get/getUsers.js
+++ controller/users/get/getUsers.js
@@ -1,5 +1,6 @@
 const getUsers = (req, res) => {
-  res.json({ url: "desde controller user get" });
+  console.log("desde el controller");
+  res.send.json({ url: "desde controller user get" });
 };
 
 export { getUsers };
